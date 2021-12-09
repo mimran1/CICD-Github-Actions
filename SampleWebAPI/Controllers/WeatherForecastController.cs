42a591282a32caed397d9daae94bada7b5e925d4
Mohammad <imran2408@gmail.com>
2021-12-09 15:24:45 -0500

    Updated WeatherForcast.cs

--- SampleWebAPI/Controllers/WeatherForecastController.cs
+++ SampleWebAPI/Controllers/WeatherForecastController.cs
@@ -13,7 +13,7 @@ namespace SampleWebAPI.Controllers
     {
         private static readonly string[] Summaries = new[]
         {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
+            "Freezing", "Bracing", "CHIIIIIILLLLLLYYYYYY", "Cool", "Mild", "WAAAAAAAAAARRRRRRRRRMMMMMMMMMM", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
         private readonly ILogger<WeatherForecastController> _logger;
